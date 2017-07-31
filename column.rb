70af8752d7d206fc99c5ece1904c28047fe242b4
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-07-31 17:27:07 -0500

    column.rb now rubcop compliant

--- column.rb
+++ column.rb
@@ -1,3 +1,4 @@
+# a column of tiles on the board
 class Column < NineGroup
   attr_reader :x_axis
   def initialize(x_axis, board)
@@ -5,7 +6,8 @@ class Column < NineGroup
     @x_axis = x_axis
     super
   end
+
   def find_tiles
-    @board.tiles.select {|tile| tile.x_axis == self.x_axis}
+    @board.tiles.select { |tile| tile.x_axis == @x_axis }
   end
 end
