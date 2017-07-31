ca7518415dfa854177c2cb6c0d5cc188ca0e37a1
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-07-31 18:00:49 -0500

    innersquare.rb now rubcop compliant

--- mustcontain.rb
+++ mustcontain.rb
@@ -1,8 +1,6 @@
+#This is to be used in situations where one of a given set of tiles must be a
+# => particular integer.
 class MustContain
-=begin
-This is to be used in situations where one of a given set of tiles must be a
-particular integer.
-=end
   attr_accessor :int, :tiles, :solved, :ninegroup
   def initialize(int, tiles) # tiles should be a set
     @int = int
@@ -10,14 +8,17 @@ particular integer.
     raise "Has #{@tiles.count} tiles instead of 9" unless @tiles.count == 9
     @candidate_tiles = @tiles.dup
   end
+
   def ==(other)
     other.int == @int && other.tiles == @tiles
   end
+
   def eql?(other)
     # not sure if I'll need this, but might as well make it while I'm thinking
-    # of it
+    # => of it
     other.instance_of?(self.class) && other == self
   end
+
   def update
     @progress = false
     return @progress if @solved # will return false
@@ -32,6 +33,7 @@ particular integer.
     end
     @progress
   end
+  
   def solution_found
     @tiles.each do |tile|
       tile.update_int_possibilities([@int]) unless tile.int == @int
