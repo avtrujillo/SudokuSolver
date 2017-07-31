5598ebd37644a86b0180e2bf0d93c7b0d63efcf2
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-07-31 18:46:50 -0500

    solution.rb now rubcop compliant

--- solution.rb
+++ solution.rb
@@ -1,39 +1,41 @@
+# included in Board to find a solution to a sudoku puzzle represented
+# => by a Board object
 module Solution
   def solved?
-    (@tiles.select {|tile| tile.int.nil?}).empty?
+    (@tiles.select { |tile| tile.int.nil? }).empty?
   end
+
   def solve
     solution_state = nil
-    until self.solved?
-      self.print_board if @tiles.map {|tile| tile.int} != solution_state
-      solution_state = @tiles.map {|tile| tile.int}
-      progress = self.solution_iteration
-      raise "intractable" unless progress
+    until solved?
+      print_board unless @tiles.map(&int) == solution_state
+      solution_state = @tiles.map(&int)
+      progress = solution_iteration
+      raise 'intractable' unless progress
     end
-    self.print_board
-    puts "Solved!"
+    print_board
+    puts 'Solved!'
   end
+
   def solution_iteration
-    progress = nil
-    bar = @mustcontains.find do |mc|
-      mc.ninegroup.is_a?(InnerSquare) &&
-      mc.int == 9 &&
-      mc.ninegroup.x_axis_range == (4..6) &&
-      mc.ninegroup.y_axis_range == (4..6)
-    end
+    iterate_mustcontains || iterate_tiles || nil
+  end
+
+  def iterate_mustcontains
     @mustcontains.each do |mc|
       progress = mc.update
       return progress if progress
     end
+    nil
+  end
+
+  def iterate_tiles
     @tiles.each do |tile|
       if tile.int_possibilities.count == 1
-        byebug if tile.int_possibilities.first == 9
-        byebug if tile.x_axis == 6 && tile.y_axis == 5
         tile.int = tile.int_possibilities.first
-        progress = :narrow_int_possibilites
-        return progress
+        return :narrow_int_possibilites
       end
     end
-    progress
+    nil
   end
 end
