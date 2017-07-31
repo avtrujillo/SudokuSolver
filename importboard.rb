63d15473785b55e259bd2cd0af8f290c92be8904
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-07-31 17:50:48 -0500

    importboard.rb now rubcop compliant

--- importboard.rb
+++ importboard.rb
@@ -1,26 +1,30 @@
+# used to generate a Board object from input.txt
 module ImportBoard
-  def get_board_lines
+  def board_lines_from_file
     Dir.chdir(__dir__)
-    raise unless File.exist?("input.txt")
-    board_lines = IO.readlines("input.txt")
+    raise unless File.exist?('input.txt')
+    IO.readlines('input.txt')
   end
+
   def remove_horizontal_bars(board_lines)
-    board_lines.select {|line| line[0] != "+"}
+    board_lines.reject { |line| line[0] == '+' }
   end
+
   def int_rows_from_lines(board_lines)
     board_lines.map do |line|
-      line.chars.inject(Array.new) do |ints, char|
-        if (1..9).include?(char.to_i)
+      line.chars.each_with_object([]) do |char, ints|
+        if (1..9).cover?(char.to_i)
           ints << char.to_i
-        elsif char == "_"
+        elsif char == '_'
           ints << nil
         end
         ints
       end
     end
   end
+
   def create_tiles_from_input(int_rows)
-    tiles = Array.new
+    tiles = []
     int_rows.each_with_index do |row, y|
       row.each_with_index do |int, x|
         tiles << Tile.new((x + 1), (y + 1), int, nil)
@@ -28,13 +32,14 @@ module ImportBoard
     end
     tiles.flatten
   end
+
   def create_board_from_input
-    board_lines = self.get_board_lines
-    board_rows = self.remove_horizontal_bars(board_lines)
-    int_rows = self.int_rows_from_lines(board_rows)
-    tiles = self.create_tiles_from_input(int_rows)
+    board_lines = board_lines_from_file
+    board_rows = remove_horizontal_bars(board_lines)
+    int_rows = int_rows_from_lines(board_rows)
+    tiles = create_tiles_from_input(int_rows)
     board_from_input = Board.new(tiles)
-    board_from_input.tiles.each {|tile| tile.board = board_from_input}
+    board_from_input.tiles.each { |tile| tile.board = board_from_input }
     board_from_input
   end
 end
