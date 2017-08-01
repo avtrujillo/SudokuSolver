e0649eada11ea4c484641f22bbeb3bdb94349b83
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-07-31 19:09:47 -0500

    all files except mustcontain.rb now rubocop compliant

--- objects/tile.rb
+++ objects/tile.rb
@@ -1,3 +1,4 @@
+# a tile on a board in a sudoku game
 class Tile
   ONE_THRU_NINE = (1..9).to_set
   attr_reader :x_axis, :y_axis, :int_possibilities
@@ -7,33 +8,32 @@ class Tile
     @x_axis = x_axis
     @y_axis = y_axis
     @board = board
-    if @int
-      @int_possibilities = Set.new
-    else
-      @int_possibilities = (1..9).to_set
-    end
+    @int_possibilities = (@int ? Set.new : (1..9).to_set)
   end
+
   def row
-    @board.rows.find {|row| row.y_axis == self.y_axis}
+    @board.rows.find { |row| row.y_axis == @y_axis }
   end
+
   def column
-    @board.columns.find {|column| column.x_axis == self.x_axis}
+    @board.columns.find { |column| column.x_axis == @x_axis }
   end
+
   def inner_square
     @board.inner_squares.find do |square|
       raise "InnerSquare doesn't have nine tiles" unless square.tiles.count == 9
       square.tiles.include?(self)
     end
   end
-  def update_int_possibilities(array_or_set_of_ints_to_be_removed)
-    byebug if @x_axis == 6 && @y_axis == 5 && array_or_set_of_ints_to_be_removed.include?(2)
-    @int_possibilities -= array_or_set_of_ints_to_be_removed
+
+  # int_enum is an array or set of ints to be removed
+  def update_int_possibilities(int_enum)
+    @int_possibilities -= int_enum
     if @int_possibilities.count == 1
       @int = @int_possibilities.first
       @int_possibilities = Set.new
-    elsif
-      @int_possibilities.count == 0 && !@int
-      raise "zero possible integers for tile"
+    elsif @int_possibilities.count.zero? && !@int
+      raise 'zero possible integers for tile'
     end
   end
 end
